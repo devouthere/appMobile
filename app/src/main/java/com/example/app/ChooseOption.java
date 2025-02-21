a88a96072fc650580a0336f700c178ca98e1406f
Emilio <emiliocantil726@gmail.com>
2025-02-20 21:13:17 -0300

    criando tela de escolher opcoes, e animaçóes

---
+++ app/src/main/java/com/example/app/ChooseOption.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooseOption extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_option);
+    }
+}
