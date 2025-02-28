19c3c667456ce6eec7061a96ff6ab14793585623
Emilio <emiliocantil726@gmail.com>
2025-02-28 18:50:55 -0300

    seprando telas do layout de sendcode e verifycodewsms

--- app/src/main/java/com/example/app/VerifyCodeActivity.java
+++ app/src/main/java/com/example/app/VerifyCodeActivity.java
@@ -22,6 +22,7 @@ public class VerifyCodeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_verify_code);
 
         mAuth = FirebaseAuth.getInstance();
