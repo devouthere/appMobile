6d9c6f055317c3f63e4ef2a52675cf1d029df73a
Fernando-Accacio <fernando.accacio15@gmail.com>
2025-04-14 00:12:57 -0300

    Adicionando opção de voltar na tela de verificação de código

--- app/src/main/java/com/example/app/view/VerifyCodeActivity.java
+++ app/src/main/java/com/example/app/view/VerifyCodeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -32,6 +33,14 @@ public class VerifyCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_code);
+
+        ImageView backArrow = findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(v -> {
+            Intent intent = new Intent(VerifyCodeActivity.this, SendCodeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        });
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
