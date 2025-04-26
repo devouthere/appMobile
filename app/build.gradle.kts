319e5381b06421f7c8dd5b252b82b81a7dea986d
Emilio <emiliocantil726@gmail.com>
2025-04-25 23:45:31 -0300

    removendo chave

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "visionary.gobarber"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         vectorDrawables.useSupportLibrary = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
